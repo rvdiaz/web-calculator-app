7c1682360cacef5b5dc0982db5bf0be38f5b62bb
rvdiaz <valdesdiazroberto@gmail.com>
2023-05-30 17:23:04 -0400

    fixing result bug

--- src/slices/calculatorSlice.js
+++ src/slices/calculatorSlice.js
@@ -17,13 +17,13 @@ const calculatorSlice=createSlice({
             state.result='0';
         },
         numberSelections(state,newValue){
-            if(state.operation==''){
-                if(state.result=='0')
+            if(state.operation===''){
+                if(state.result==='0')
                     state.result=newValue.payload;
                 else
                     state.result=state.result+''+newValue.payload;
             }else {
-                if(state.secondOperator==''){
+                if(state.secondOperator===''){
                     state.result=newValue.payload;
                     state.secondOperator=parseFloat(state.result);
                 }
@@ -35,17 +35,21 @@ const calculatorSlice=createSlice({
         },
         oppositeCalculator(state){
             const resAux=parseFloat(state.result);
-            if(state.result!='0'){
+            if(state.result!=='0'){
+                if(state.secondOperator!==''){
+                    state.result='' + -resAux;
+                    state.secondOperator=parseFloat(state.result);
+                }else
                 state.result='' + -resAux;
             }
         },
         percentCalculator(state,newValue){
             const resAux=parseFloat(state.result);
-            if(state.result!='0'){
+            if(state.result!=='0'){
                 state.result='' + resAux/100;
             } 
             else{
-                if(state.secondOperator==''){
+                if(state.secondOperator===''){
                     state.result=newValue.payload;
                     state.secondOperator=parseFloat(state.result);
                 }
@@ -57,12 +61,13 @@ const calculatorSlice=createSlice({
             }
         },
         addingOperation(state,operation){
-            if(operation.payload!='='){
-                if(state.secondOperator==''){
+            if(operation.payload!=='='){
+                if(state.secondOperator===''){
                     state.operation=operation.payload;
                     state.firstOperator=parseFloat(state.result);
             }
             }else{
+                if(state.firstOperator!=='' && state.secondOperator!=='' && state.operation!=='')
                 state.result=getResult(state.firstOperator,state.secondOperator,state.operation);
                 state.firstOperator='';
                 state.secondOperator='';
